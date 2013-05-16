74b757650264ab5cbe5db874af478f0083cda431
Sherod Taylor <sherodtaylor@gmail.com>
2013-05-16 16:17:08 -0700

    responds to .json now

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -2,6 +2,8 @@ class ApiController < ApplicationController
   def api
     user_params = params[:user]
     @top_artists = Search.formatted_code(user_params)
-    render :json => @top_artists
+    respond_to do |format|
+      format.json  { render :json => @top_artists }
+    end
   end
 end
